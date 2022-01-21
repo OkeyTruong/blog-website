92c26aea95159f47ec464628eb3beca98882f065
huy <okey222k@gmail.com>
2022-01-21 18:18:28 +0700

    design page settings

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Topbar from "./components/topbar/Topbar";
 import Home from "./pages/home/Home";
+import Settings from "./pages/setting/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
 
@@ -7,8 +8,10 @@ function App() {
   return (
     <>
       <Topbar />
+      {/* <Home/> */}
       {/* <Single/> */}
-      <Write/>
+      {/* <Write/> */}
+      <Settings/>
     </>
   );
 }
---
+++ src/pages/setting/Settings.jsx
@@ -0,0 +1,36 @@
+import Sidebar from "../../components/sidebar/Sidebar";
+import "./settings.css";
+
+export default function Settings() {
+  return (
+    <div className="settings">
+      <div className="settingsWrapper">
+        <div className="settingsTitle">
+          <span className="settingsTitleUpdate">update your account</span>
+          <span className="settingsTitleDelete">delete account</span>
+        </div>
+        <form action="" className="settingsForm">
+          <label>Profile Picture</label>
+          <div className="settingsPP">
+            <img
+              src="https://steemitimages.com/0x0/https://steemitimages.com/DQmaqkEZZpvCytYVUi6e3UkUj1CankKPz1MsUyuAxKuyoS7/8816995.gif"
+              alt=""
+            />
+            <label htmlFor="fileInput">
+              <i className="settingsPPIcon far fa-user-circle"></i>
+            </label>
+            <input type="file" id="fileInput" style={{ display: "none" }} />
+          </div>
+          <label htmlFor="">username</label>
+          <input type="text" placeholder="Okey" />
+          <label htmlFor="">email</label>
+          <input type="email" placeholder="okey222k@gmail.com" />
+          <label htmlFor="">password</label>
+          <input type="password" />
+          <button className="settingsSubmitButton">update</button>
+        </form>
+      </div>
+      <Sidebar />
+    </div>
+  );
+}
