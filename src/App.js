e5fe8dec4ba7ea48b33684c052dd8b7bf1e105a2
huy <okey222k@gmail.com>
2022-01-21 15:33:48 +0700

    degin write

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Topbar from "./components/topbar/Topbar";
 import Home from "./pages/home/Home";
 import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
 
 function App() {
   return (
     <>
       <Topbar />
-      <Single/>
+      {/* <Single/> */}
+      <Write/>
     </>
   );
 }
---
+++ src/pages/write/Write.jsx
@@ -0,0 +1,34 @@
+import "./write.css";
+export default function Write() {
+  return (
+    <div className="write">
+      <img
+        className="writeImg"
+        src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8ZGV2ZWxvcGVyfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+        alt=""
+      />
+      <form action="" className="writeForm">
+        <div className="writeFormGroup">
+          <label htmlFor="fineInput">
+            <i className="writeIcon fas fa-plus"></i>
+          </label>
+          <input type="file" id="fileInput" style={{ display: "none" }} />
+          <input
+            type="text"
+            placeholder="Title"
+            className="writeInput"
+            autoFocus={true}
+          />
+        </div>
+        <div className="writeFormGroup">
+          <textarea
+            placeholder="Tell your story..."
+            type="text"
+            className="writeInput writeText"
+          ></textarea>
+        </div>
+        <button className="writeSubmit">Publish</button>
+      </form>
+    </div>
+  );
+}
