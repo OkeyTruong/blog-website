e53a70f11bd2bd0d86754c2b697d71b37bf81b15
huy <okey222k@gmail.com>
2022-01-23 22:34:18 +0700

    upload file

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ const authRouter = require("./routes/auth");
 const userRouter = require("./routes/users");
 const postRouter = require("./routes/posts");
 const categoryRouter = require("./routes/categories")
+const multer = require("multer")
 
 const app = express();
 
@@ -20,6 +21,21 @@ mongoose
   })
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
+
+const storage = multer.diskStorage({
+  destination:(req, file, cb) =>{
+    cb(null, "images")
+  },filename:(req, file, cb)=>{
+    cb(null,req.body.name)
+  }
+})
+
+const upload = multer({
+  storage:storage
+})
+app.post("/api/upload", upload.single("file"),(req,res)=>{
+  res.status(200).json("File has been uploaded")
+})
 
 app.use("/api/auth",authRouter)
 app.use("/api/users",userRouter)
