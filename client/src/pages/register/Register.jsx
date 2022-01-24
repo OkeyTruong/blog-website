b7f2b641aea691a9b506c879e976a3e64042c6d4
huy <okey222k@gmail.com>
2022-01-24 19:21:28 +0700

    regsiter system

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,21 +1,65 @@
-import { Link } from "react-router-dom"
-import "./register.css"
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./register.css";
+import axios from "axios";
 
 export default function Register() {
-    return(
-        <div className="register">
-            <span className="registerTitle">register</span>
-            <form action="" className="registerForm">
-                <label >email</label>
-                <input type="email" className="registerInput" placeholder="Enter your email..."/>
-                <label >password</label>
-                <input type="password" className="registerInput" placeholder="Enter your password..."/>
-            <button className="registerButton">register</button>
-            </form>
-            <button className="registerLoginButton">
-                <Link className="link" to='/login'>login</Link>
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false)
 
-            </button>
-        </div>
-    )
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(false)
+    try {
+      const res = await axios.post("/auth/register", {
+        username,
+        email,
+        password,
+      });
+      res.data && window.location.replace("/login")
+    } catch (error) {
+      console.log(error);
+      setError(true)
+    }
+  };
+
+  return (
+    <div className="register">
+      <span className="registerTitle">register</span>
+      <form action="" className="registerForm" onSubmit={handleSubmit}>
+        <label>Username</label>
+        <input
+          type="text"
+          className="registerInput"
+          placeholder="Enter your Username..."
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label>email</label>
+        <input
+          type="email"
+          className="registerInput"
+          placeholder="Enter your email..."
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label>password</label>
+        <input
+          type="password"
+          className="registerInput"
+          placeholder="Enter your password..."
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className="registerButton" type="submit">
+          register
+        </button>
+      </form>
+      <button className="registerLoginButton">
+        <Link className="link" to="/login">
+          login
+        </Link>
+      </button>
+      {error && <span className="alertError">Something went wrong!</span>}
+    </div>
+  );
 }
